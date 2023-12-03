b80e8430e7fcd8d63f1f796d366bc30643512bdb
Kartikey Shukla <kartikey8604@gmail.com>
2023-12-03 23:46:03 +0530

    setup basic express server

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
-import 'colors';
 import { DB_NAME } from '../constants.js';
 
 const dbConnect = async () => {
   try {
     const conn = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
     console.log(
-      `MongoDB Connected successfully at host: ${conn.connection.host}`.magenta
-        .underline
+      `MongoDB connected at host: ${conn.connection.host}`.magenta.underline
     );
   } catch (error) {
     console.log('Error occurred in DB connection', error);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
 import dbConnect from './config/db.js';
+import app from './app.js';
+import 'colors';
 
 dotenv.config();
 
-dbConnect();
+const port = process.env.PORT || 5000;
+
+dbConnect().then(() => {
+  app.listen(port, () => {
+    console.log(
+      `${process.env.NODE_ENV} server is running on port:${port}`.blue.underline
+    );
+  });
+});
