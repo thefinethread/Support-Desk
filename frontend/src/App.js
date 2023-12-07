51cbf262dca81c263e7df2ef36db61317e26eb0e
Kartikey Shukla <kartikey8604@gmail.com>
2023-12-07 23:53:35 +0530

    initial commit for frontend reactsetup with basic packages

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
